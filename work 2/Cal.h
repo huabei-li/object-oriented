197f26d1a655218c24bd73de594c2e81bfb6eb3c
huabei-li <lixiang71908@hotmail.com>
2017-04-26 17:07:36 +0800

    计算面积和头文件

---
+++ work 2/Cal.h
@@ -0,0 +1,7 @@
+#define pi 3.14
+float CalCircleArea(int n)   //用来算圆的面积 
+{
+	float area;
+	area=pi*n*n; //n为半径，pi假定为3.14 
+	return area;
+}
---
+++ work 2/calculate.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+#include<D:\huabei-li\object-oriented\work 2\Cal.h> //用来存关于计算用的函数 
+using namespace std;
+int main()
+{
+	float n,area;  
+	cin >> n;   //输入半径 
+	area=CalCircleArea(n);   //调用计算圆面积的函数 ，返回值为浮点型 
+	cout << setiosflags(ios::fixed) << setprecision(2) << area;  //假定需要输出两位小数 
+	return 0;
+}
